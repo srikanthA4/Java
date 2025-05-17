395e14d2f25e423c075b236347ec00cf2d4a404c
srikanthA4 <ksreddy5a4@gmail.com>
2025-05-17 20:01:22 +0530

    Added AddFiveNumbersDirect program

---
+++ Java Fundamentals/Java Operators/AddFiveNumbersDirect.java
@@ -0,0 +1,16 @@
+public class AddFiveNumbersDirect {
+    public static void main(String[] args) {
+        // Declare and initialize five numbers
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = 15;
+        int num5 = 25;
+
+        // Calculate sum
+        int sum = num1 + num2 + num3 + num4 + num5;
+
+        // Print result
+        System.out.println("Sum of the 5 numbers is: " + sum);
+    }
+}
